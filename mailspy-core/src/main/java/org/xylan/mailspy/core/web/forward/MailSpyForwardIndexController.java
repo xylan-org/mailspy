86b73bd767b9078670b37fb24374d7bf9581b2d4
Abel Keszei <keszeiabel@gmail.com>
2021-09-26 20:55:25 +0200

    optimize dependencies of core module

--- mailspy-core/src/main/java/org/xylan/mailspy/core/web/forward/MailSpyForwardIndexController.java
+++ mailspy-core/src/main/java/org/xylan/mailspy/core/web/forward/MailSpyForwardIndexController.java
@@ -1,15 +1,14 @@
 package org.xylan.mailspy.core.web.forward;
 
 import lombok.Setter;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.xylan.mailspy.core.config.MailSpyProperties;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class MailSpyForwardIndexController {
@@ -19,7 +18,8 @@ public class MailSpyForwardIndexController {
     private MailSpyProperties properties;
 
     @RequestMapping
-    public void forwardIndexRequest(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+    @SneakyThrows
+    public void forwardIndexRequest(final HttpServletRequest request, final HttpServletResponse response) {
         request.getServletContext()
                 .getRequestDispatcher(properties.getPathNoTrailingSlash() + "/resources/index.html")
                 .forward(request, response);
