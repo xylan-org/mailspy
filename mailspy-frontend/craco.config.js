89eb00cb298de88a8e4474ea1fd615cc511b4c8d
Abel Keszei <keszeiabel@gmail.com>
2022-01-23 16:17:49 +0100

    introduce version catalog and maven publishing

--- mailspy-frontend/craco.config.js
+++ mailspy-frontend/craco.config.js
@@ -1,7 +1,14 @@
+const path = require("path");
 module.exports = {
     babel: {
         plugins: [
             "babel-plugin-transform-typescript-metadata"
         ]
+    },
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            paths.appBuild = webpackConfig.output.path = path.resolve("build", "webpack");
+            return webpackConfig;
+        }
     }
 }
