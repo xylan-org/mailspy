af982e29cfbcfbc6a287550553e32b1ab1e648c4
Abel Keszei <keszeiabel@gmail.com>
2022-05-02 22:24:21 +0200

    refine pmd rules

--- mailspy-app/src/main/java/org/xylan/mailspy/app/MailSpyApplication.java
+++ mailspy-app/src/main/java/org/xylan/mailspy/app/MailSpyApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Entry point of the standalone MailSpy application.
  */
 @SpringBootApplication
-public final class MailSpyApplication {
-
-    private MailSpyApplication() {}
+public class MailSpyApplication {
 
     /**
      * Entry point.
