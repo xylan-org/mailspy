e6c7dd52688b20553c4dc6527500d182082d8523
Abel Keszei <keszeiabel@gmail.com>
2022-12-18 12:38:20 +0100

    move integration tests to correct directory and fix index tests

--- mailspy-core/src/main/java/org/xylan/mailspy/core/config/base/MailSpyWebMvcConfig.java
+++ mailspy-core/src/main/java/org/xylan/mailspy/core/config/base/MailSpyWebMvcConfig.java
@@ -25,7 +25,9 @@ package org.xylan.mailspy.core.config.base;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.xylan.mailspy.core.config.MailSpyProperties;
@@ -62,10 +64,17 @@ public class MailSpyWebMvcConfig {
         }
 
         @Override
+        public void configurePathMatch(final PathMatchConfigurer configurer) {
+            configurer.addPathPrefix(
+                    properties.getPathNoTrailingSlash(), HandlerTypePredicate.forBasePackage("org.xylan.mailspy.core"));
+        }
+
+        @Override
         public void addCorsMappings(CorsRegistry registry) {
             if (properties.isEnableCors()) {
                 registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*");
             }
         }
+
     }
 }
